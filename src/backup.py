e866067aaba028379fea913d701b49b581008eeb
Sidney <sidneyjunior26@gmail.com>
2024-01-27 21:37:42 -0300

    remove personal data

--- src/backup.py
+++ src/backup.py
@@ -5,20 +5,20 @@ import logging
 import zipfile
 from datetime import datetime
 
-DB_HOST = ''
-DB_USER = ''
-DB_PASSWORD = ''
-DB_NAME = ''
+DB_HOST = 'localhost'
+DB_USER = 'your_username'
+DB_PASSWORD = 'your_password'
+DB_NAME = 'your_database_name'
 
-BACKUP_DIR = 'C:\\Backup-py\\backups'
+BACKUP_DIR = 'your_backup_files_path'
 DATE_FORMAT = '%Y-%m-%d_%H-%M-%S'
-MAX_BACKUP_AGE = 7  # Número de dias para manter os backups
+MAX_BACKUP_AGE = 7
 
 BACKUP_FILE = ''
 BACKUP_FILE_PATH = ''
 
-# Configurando o logging
-log_file_path = 'C:\\Backup-py\\Logs'
+# Configuring o logging
+log_file_path = 'your_log_files_path'
 logging.basicConfig(filename=log_file_path, level=logging.ERROR, format='%(asctime)s [%(levelname)s]: %(message)s')
 
 def create_backup():
@@ -27,28 +27,22 @@ def create_backup():
     BACKUP_FILE_PATH = f'{BACKUP_DIR}\\{BACKUP_FILE}'
     
     mysqldump_cmd = f'mysqldump -h {DB_HOST} -u {DB_USER} -p{DB_PASSWORD} {DB_NAME} > {BACKUP_FILE_PATH}'
-    print(mysqldump_cmd)
-    input()
 
     try:
         subprocess.run(mysqldump_cmd, shell=True, check=True)
         
         print(f"Backup created successfully: {BACKUP_FILE_PATH}")
-        input()
     except subprocess.CalledProcessError as e:
         error_message = f"Error creating backup: {e}"
         print(error_message)
         logging.error(error_message)
 
-def compress_backup(backup_file_path):
+def compress_backup():
     try:
-        print('compress_backup')
-        input()
         current_time = time.strftime(DATE_FORMAT)
         zip_file_path = f'{BACKUP_DIR}\\backup_{current_time}.zip'
 
         with zipfile.ZipFile(zip_file_path, 'w') as zip:
-          # Adicione os arquivos que deseja compactar ao arquivo ZIP
           zip.write(BACKUP_FILE_PATH)
 
         os.remove(BACKUP_FILE_PATH)
@@ -75,9 +69,8 @@ def remove_old_backups():
 
 def main():
     try:
-        input()
         create_backup()
-        compress_backup(os.path.join(BACKUP_DIR, f'{DB_NAME}-{time.strftime(DATE_FORMAT)}.sql'))
+        compress_backup()
         remove_old_backups()
     except Exception as e:
         error_message = f"Unexpected error: {e}"
